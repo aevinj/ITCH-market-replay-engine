05885e72e479ebf47de57763902965980f3b9f3d
aevinj <ajjaevinjais@gmail.com>
2025-11-28 16:29:22 +0000

    safer file reading + dropping test stocks

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,5 @@
 #include "LimitOrderBook.h"
-#include "MatchingEngine.h" 
+#include "MatchingEngine.h"
 #include <fstream>
 #include <iostream>
 #include <cstdint>
@@ -11,45 +11,62 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     ifstream file("../../12302019.NASDAQ_ITCH50", ios::binary);
     std::ofstream trade_file("aapl_trades.csv");
     trade_file << "seq,symbol,taker,maker,price,quantity" << endl;
     static uint64_t trade_seq = 0;
 
-    if (!file) {
+    if (!file)
+    {
         cerr << "Failed to open the required ITCH file" << endl;
         return 1;
     }
 
     const unordered_set<string> tracked_symbols = {
-        "AAPL","MSFT","AMZN","GOOGL","META","NVDA","TSLA","ORCL","INTC","AMD",
-        "JPM","BAC","GS","MS","WMT","COST","TGT","NFLX","DIS","NKE"
-    };
+        "AAPL", "MSFT", "AMZN", "GOOGL", "META", "NVDA", "TSLA", "ORCL", "INTC", "AMD",
+        "JPM", "BAC", "GS", "MS", "WMT", "COST", "TGT", "NFLX", "DIS", "NKE"};
 
     unordered_map<uint16_t, string> locate_to_symbol;
 
     unordered_map<uint16_t, MatchingEngine *> locate_to_engine;
     size_t tracked_symbols_count = 0;
 
-    while (file) {
-        uint16_t length;
-        file.read(reinterpret_cast<char*>(&length), sizeof(length));
+    while (file)
+    {
+        uint16_t length_be;
+        if (!file.read(reinterpret_cast<char *>(&length_be), sizeof(length_be)))
+        {
+            break; // EOF or error
+        }
 
-        // Big Endian conversion
-        length = (length >> 8) | (length << 8);
+        uint16_t length = (length_be >> 8) | (length_be << 8);
+        if (length < 1)
+        {
+            std::cerr << "Invalid length: " << length << "\n";
+            break;
+        }
 
         char type;
-        file.read(&type, 1);
+        if (!file.read(&type, 1))
+        {
+            cerr << "Failed to read type\n";
+            break;
+        }
 
         // not using char array due to VLA (variable length assignment - silent error)
         vector<char> payload(length - 1);
-
-        file.read(payload.data(), payload.size());
+        if (!file.read(payload.data(), payload.size()))
+        {
+            cerr << "Failed to read payload, len=" << (length - 1) << "\n";
+            break;
+        }
 
         uint16_t stock_locate = (payload[0] << 8) | payload[1];
 
-        if (type == 'R') {
+        if (type == 'R')
+        {
             char symbol[9]{};
             memcpy(symbol, payload.data() + 10, 8);
             symbol[8] = '\0';
@@ -58,7 +75,21 @@ int main() {
             while (!stock.empty() && stock.back() == ' ')
                 stock.pop_back();
 
-            if (stock.empty() || !tracked_symbols.count(stock) || locate_to_engine.count(stock_locate)) continue;
+            if (stock.empty() || !tracked_symbols.count(stock) || locate_to_engine.count(stock_locate))
+                continue;
+
+            char market_category = payload[18];
+            char financial_status = payload[19];
+            char issue_classification = payload[25];
+            char authenticity = payload[28];
+
+            // Only production, normal, common stock
+            if (authenticity != 'P')
+                continue; // drop test issues
+            if (issue_classification != 'C')
+                continue; // only common stock
+            if (financial_status != 'N' && financial_status != ' ')
+                continue;
 
             locate_to_symbol[stock_locate] = stock;
 
@@ -66,7 +97,8 @@ int main() {
             MatchingEngine *engine = new MatchingEngine(*lob);
 
             string stock_copy = stock;
-            engine->setTradeCallback([&trade_file, stock_copy](const TradeEvent& ev) {
+            engine->setTradeCallback([&trade_file, stock_copy](const TradeEvent &ev)
+                                     {
                 std::cout << "TRADE taker=" << ev.taker_id
                         << " maker=" << ev.maker_id
                         << " qty="   << ev.quantity
@@ -80,15 +112,16 @@ int main() {
                         << ev.taker_id << ","
                         << ev.maker_id << ","
                         << ev.price << ","
-                        << ev.quantity << endl;
-            });
+                        << ev.quantity << endl; });
             locate_to_engine[stock_locate] = engine;
             continue;
         }
 
-        if (locate_to_engine.count(stock_locate)) {
+        if (locate_to_engine.count(stock_locate))
+        {
             MatchingEngine *engine = locate_to_engine[stock_locate];
-            if (type == 'A') {
+            if (type == 'A' || type == 'F')
+            {
                 uint64_t order_id = 0;
                 for (int i = 0; i < 8; i++)
                     order_id = (order_id << 8) | (unsigned char)payload[10 + i];
@@ -111,26 +144,30 @@ int main() {
 
                 engine->submitLimit(order_id, side, price, shares);
             }
-            else if (type == 'D') {
+            else if (type == 'D')
+            {
                 uint64_t order_id = 0;
                 for (int i = 0; i < 8; i++)
                     order_id = (order_id << 8) | (unsigned char)payload[10 + i];
 
                 engine->cancel(order_id);
             }
-            else if (type == 'X') {
+            else if (type == 'X')
+            {
                 uint64_t order_id = 0;
                 for (int i = 0; i < 8; i++)
                     order_id = (order_id << 8) | (unsigned char)payload[10 + i];
 
                 uint32_t cancelled_shares = 0;
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++)
+                {
                     cancelled_shares = (cancelled_shares << 8) | (unsigned char)payload[18 + i];
                 }
 
                 engine->reduce_order(order_id, cancelled_shares);
             }
-            else if (type == 'U') {
+            else if (type == 'U')
+            {
                 uint64_t old_order_id = 0;
                 for (int i = 0; i < 8; i++)
                     old_order_id = (old_order_id << 8) | (unsigned char)payload[10 + i];
@@ -140,25 +177,25 @@ int main() {
                     new_order_id = (new_order_id << 8) | (unsigned char)payload[18 + i];
 
                 uint32_t qty = 0;
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++)
                     qty = (qty << 8) | (unsigned char)payload[26 + i];
-                }
 
                 uint32_t price = 0;
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++)
                     price = (price << 8) | (unsigned char)payload[30 + i];
-                }
 
                 double px = price / 10000.0;
                 engine->order_replace(old_order_id, new_order_id, px, qty);
             }
-            else if (type == 'E') {
+            else if (type == 'E')
+            {
                 uint64_t order_id = 0;
                 for (int i = 0; i < 8; i++)
                     order_id = (order_id << 8) | (unsigned char)payload[10 + i];
 
                 uint32_t executed_shares = 0;
-                for (int i = 0; i < 4; i++) {
+                for (int i = 0; i < 4; i++)
+                {
                     executed_shares = (executed_shares << 8) | (unsigned char)payload[18 + i];
                 }
 
